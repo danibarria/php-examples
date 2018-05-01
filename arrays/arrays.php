7228b4ac28de2b0bf19ec7ec4476b3a032110d77
DanielBarria <barriareyesjosedaniel@gmail.com>
2018-05-01 11:34:20 -0300

    arrays php

---
+++ arrays/arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$array1 = [
+	0 => 'a',
+	1 => 'b',
+	2 => 'c'
+];
+
+$array2 =[
+	3 => 'd',
+	4 => 'e'
+];
+
+$result = $array1 + $array2;
+
+var_dump($result);
+
+?>
---
+++ arrays/index.php
@@ -0,0 +1,10 @@
+<?php
+$myArray = [
+	'color1' => 'red',
+	'color2' => 'blue',
+	3 => 'black'
+	];
+
+var_dump($myArray);
+var_dump($myArray['color1']);
+?>
