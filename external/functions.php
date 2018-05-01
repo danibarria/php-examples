4d23aeae7fa90bb726d1c7dc27e06da2fa09c184
DanielBarria <barriareyesjosedaniel@gmail.com>
2018-05-01 11:35:31 -0300

    agregando archivos php

---
+++ external/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+function suma($op1, $op2){
+	return $op1 + $op2;
+}
+function hello(){
+	return 'Hello World';
+}
+function sayHi($name){
+	return 'Hello ' . $name;
+}
+function helloWorld(){
+	echo 'Hello';
+}
+function printNewLine(){
+	echo '<br>';
+}
+
+
+echo suma(1,2);
+printNewLine();
+echo hello();
+printNewLine();
+echo sayHi('Dani');
+printNewLine();
+helloWorld();
+
+
+// if	else
+// switch case default
+?>
---
+++ external/index.php
@@ -0,0 +1,9 @@
+<?php 
+include 'functions.php'; //si el archivo no se carga, sigue
+// require 'functions.php'; //si el archivo no se carga, pincha
+//con include_once, se incluye una sola vez
+//lo mismo con require_once lo msimo
+echo "<p> Text from functions.php </p>";
+
+
+?>
