32cee9c87d6ab8b21ca313d71b65d475181bc59f
DanielBarria <barriareyesjosedaniel@gmail.com>
2018-05-01 11:34:38 -0300

    operadores

---
+++ operators/index.php
@@ -0,0 +1,17 @@
+<?php
+$a =1;
+$b =3;
+
+echo $a + $b;
+// +-*/%**
+$v1=10 $v2='10';
+//si hacemos
+$result = $v1==$v2;
+//esto es verdadero
+//encambio
+$result = $v1 === $v2;
+//esto da falso
+
+// operador spaceship <=>  -1, 0 ,1 funciona como strcmp -1 si el 1ero es menor, 0 si son iguales, 1 si es 1ero es mayor
+
+?>
